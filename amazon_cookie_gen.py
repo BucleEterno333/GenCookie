99d54731a0c8763adf9e15f3732dfef4ee1d56ae
alexisgonzales2728-byte <alexisgonzales2728@gmail.com>
2026-06-07 03:14:16 -0600

    U.U

--- amazon_cookie_gen.py
+++ amazon_cookie_gen.py
@@ -2215,7 +2215,9 @@ async def get_phone_number(account_country, force_service=None, force_country=No
     # Mapeo de códigos de país a números para Hero SMS
     #CAMBIAR ORDEN?
     hero_country_map = {
-        'BR': 73,   # Brazil +55
+        'CA': 36,    # Canada +1
+        'US': 187,    # USA +1
+        'BR': 73,   # Brasil +55
         'CM': 41,   # Cameroon +237
         'MY': 7,    # Malaysia +60
         'KZ': 2,    # Kazakhstan +7
